66a0a306ed6b71613e58f3699c971e417d11c32c
agfreckles <oluueli@gmail.com>
2022-03-08 20:27:46 +0000

    Add accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
 
-function App() {
+const items = [
+  {
+    title: "What is Mathematics ?",
+    content: "Maths is good subject to study at school",
+  },
+  {
+    title: "How does Math relate to reality ?",
+    content: "Maths helps calculate daily monetary transactions",
+  },
+  { title: "How do you learn Math ?", content: "Just learn by doing " },
+];
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br />
+      <Accordion items={items} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  let [counter, setCounter] = useState(0);
+  const handleTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+  const counterClick = (counter) => {
+      setCounter(counter);
+  }
+  const renderedItems = items.map((item, i) => {
+    const active = i === activeIndex ? "active" : "";
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => handleTitleClick(i)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div
+          className={`content ${active}`}
+          onClick={() => counterClick(counter++)}
+        >
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <div>{counter}</div>
+    </div>
+  );
+};
+export default Accordion;
